28cb20f4b159f6ea75c9bfead99857a6ff126d27
itzik saban <itziksaban@gmail.com>
2023-01-10 09:31:00 +0200

    Update Task.cs

--- CleanCode/AzureDevOps/Tasks/Task.cs
+++ CleanCode/AzureDevOps/Tasks/Task.cs
@@ -4,28 +4,9 @@ namespace AzureDevOps.Tasks;
 
 public class Task
 {
-    public Task()
-    {
-
-    }
     public TaskStatus Status { get; set; }
     public DateTime StartDate { get; set; }
     public int Estimate { get; set; }
     public int FeatureId { get; set; }
     public string Name { get; set; }
-
-    public int CalcDeviation()
-    {
-        int taskDeviation = 0;
-        if (Status != TaskStatus.Closed)
-        {
-            var dueDate = StartDate.AddDays(Estimate);
-            if (dueDate > DateTime.Now)
-            {
-                taskDeviation = (int)(dueDate - DateTime.Now).TotalDays;
-            }
-        }
-
-        return taskDeviation;
-    }
 }
