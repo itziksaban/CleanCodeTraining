a5437065fe9337b58f668fad94ff691bde795631
itziksaban@gmail.com <itziksaban@gmail.com>
2024-02-23 10:57:46 +0200

    Plain employee repository

---
+++ CleanCode/Refactoring/Class1.cs
@@ -0,0 +1,19 @@
+﻿using CleanCode_playground;
+
+namespace Refactoring
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public async Task SomeMethod()
+        {
+            var repository = await EmployeeRepository.CreateConnected(3.5, 5, "conn", true, false);
+            Employee employee = null;
+            repository.Upsert(employee);
+
+        }
+    }
+}
--- CleanCode/Refactoring/EmployeeRepository.cs
+++ CleanCode/Refactoring/EmployeeRepository.cs
@@ -3,84 +3,63 @@ using StackExchange.Redis;
 
 namespace CleanCode_playground;
 
-public class SavingOptions
-{
-    public SavingOptions(int maxRetries, int ttl)
-    {
-        MaxRetries = maxRetries;
-        Ttl = ttl;
-    }
-
-    public int MaxRetries { get; private set; }
-    public int Ttl { get; private set; }
-}
-
 public class EmployeeRepository
 {
-    private readonly SavingOptions _savingOptions;
-    private readonly bool _ignoreNulls;
-    private readonly bool _ignoreCircularReferences;
     private IDatabase _database;
+    private readonly double _ttl;
+    private readonly int _maxRetries;
+    private readonly bool _ignoreNulls;
+    private bool _ignoreCircularReferences;
 
-    private EmployeeRepository(SavingOptions savingOptions, bool ignoreNulls, bool ignoreCircularReferences)
+    public EmployeeRepository(double ttl, int maxRetries, bool ignoreNulls, bool ignoreCircularReferences,
+        IDatabase database)
     {
-        _savingOptions = savingOptions;
         _ignoreNulls = ignoreNulls;
+        _ttl = ttl;
+        _maxRetries = maxRetries;
         _ignoreCircularReferences = ignoreCircularReferences;
+        _database = database;
     }
 
-    public static async Task<EmployeeRepository> CreateConnected(string connectionString, bool ignoreNulls, bool ignoreCircularReferences, 
-        SavingOptions savingOptions)
+    public static async Task<EmployeeRepository> CreateConnected(double ttl, int maxRetries, string connectionString, bool ignoreNulls, bool ignoreCircularReferences)
     {
-        var customerRepository = new EmployeeRepository(savingOptions, ignoreNulls, ignoreCircularReferences);
-        await customerRepository.Connect(connectionString);
+        var database = await Connect(connectionString);
+        var customerRepository = new EmployeeRepository(ttl, maxRetries, ignoreNulls, ignoreCircularReferences, database);
         return customerRepository;
     }
 
     public void Upsert(Employee employee)
     {
-        var json = Serialize(employee);
-        StoreInDb(employee.Id, json);
-    }
-
-    private void StoreInDb(RedisKey employeeId, string json)
-    {
-        int retries = 0;
-        do
-        {
-            try
-            {
-                _database.StringSet(employeeId, json, TimeSpan.FromSeconds(_savingOptions.Ttl));
-            }
-            catch (Exception e)
-            {
-                retries++;
-            }
-        } while (retries < _savingOptions.MaxRetries);
-    }
-
-    private async Task Connect(string connectionString)
-    {
-        var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(connectionString);
-        _database = connectionMultiplexer.GetDatabase();
-    }
-
-    private string Serialize(Employee employee)
-    {
-        var config = CreateConfig();
-        string json = JsonConvert.SerializeObject(employee, config);
-        return json;
-    }
-
-    private JsonSerializerSettings CreateConfig()
-    {
-        JsonSerializerSettings config = new JsonSerializerSettings
+        var config = new JsonSerializerSettings
         {
             NullValueHandling = _ignoreNulls ? NullValueHandling.Ignore : NullValueHandling.Include,
             ReferenceLoopHandling = _ignoreCircularReferences
                 ? ReferenceLoopHandling.Ignore
                 : ReferenceLoopHandling.Serialize
         };
-        return config;
+        var json = JsonConvert.SerializeObject(employee, config);
+        int retries = 0;
+        do
+        {
+            try
+            {
+                _database.StringSet(employee.Id, json, TimeSpan.FromSeconds(_ttl));
+            }
+            catch (Exception e)
+            {
+                retries++;
+            }
+        } while (retries < _maxRetries);
     }
+
+    private static async Task<IDatabase> Connect(string connectionString)
+    {
+        var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(connectionString);
+        return connectionMultiplexer.GetDatabase();
+    }
+}
+
+public class Employee
+{
+    public RedisKey Id { get; set; }
 }
