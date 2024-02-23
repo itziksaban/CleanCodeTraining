1792055766ea4f19d78b3a19644fba200aaaf86c
itziksaban@gmail.com <itziksaban@gmail.com>
2024-02-23 10:57:46 +0200

    Plain employee repository

---
+++ CleanCode/Refactoring/Class1.cs
@@ -0,0 +1,23 @@
+﻿using CleanCode_playground;
+using StackExchange.Redis;
+
+namespace Refactoring
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public async Task SomeMethod()
+        {
+            var repository = await EmployeeRepository.CreateConnected(3.5, 5, "conn", true, false);
+            Employee employee = new Employee
+            {
+                Id = new RedisKey(Guid.NewGuid().ToString())
+            };
+            repository.Upsert(employee);
+
+        }
+    }
+}
